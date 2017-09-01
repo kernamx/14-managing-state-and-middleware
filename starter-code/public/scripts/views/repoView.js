ee3867b3e0e1acd3568bd7f42013fc5a0abb9f10
Trey64 <thomasellison.r@gmail.com>
2017-08-31 17:19:56 -0700

    All Comments finished

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -13,7 +13,11 @@ var app = app || {};
 
   const render = Handlebars.compile($('#repo-template').text());
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // COMMENT:(DONE) What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // This function empties the DOM, shows everything with an #about and hides its siblings,
+  // and appends our articles with author names to the DOM.
+  // It is called in aboutController.js
+  // It calls the ui() function which resides here in repoView.js.
   repoView.index = function() {
     ui();
 
